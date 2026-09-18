e728ad6c2bdc80d2b264747b929e26556d69f054
Brinkley <1orbrinkley@seattleschools.org>
2026-09-18 12:40:27 -0700

    Update Practice09182026.java
    Up to date

--- Practice09182026.java
+++ Practice09182026.java
@@ -2,7 +2,14 @@ public class Practice09182026 {
 
    public static void main(String[] args) {
    
-   System.out.println(3/4);
+   //System.out.println(3/4);
+   //System.out.println(3.0 / 4);
+   //System.out.println((double) 3.0 / 4);
+   //System.out.println((int) 3.4 / 4);
+   //double numberToRound = 3.5;
+   //int roundedNumber = (int) (numberToRound + 0.5);
+   //System.out.println(roundedNumber);
+   
    
    }
 
